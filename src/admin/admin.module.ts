0387c467dc3f754b51911d8ebc5bdaa6d53a9bfb
Leonardorossato <leonardorossato21@gmail.com>
2024-03-21 09:20:38 -0300

    autenticação e autorização do usuario e do admin finalizada

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AdminService } from './admin.service';
-import { AdminController } from './admin.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+import { JwtStrategy } from '../guards/jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, JwtStrategy],
 })
 export class AdminModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,23 +6,48 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiTags } from '@nestjs/swagger';
-import { RegisterUserDto } from './dto/register-user.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { RegisterUserDto, UserRole } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { Role } from '../guards/role.guard';
+import { RolesGuard } from '../guards/role.strategy';
+import { JwtAuthGuard } from '../guards/jwt.guard';
 
 @Controller('users')
 @ApiTags('Users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Register a new user' })
   @Post('/register')
   register(@Body() dto: RegisterUserDto) {
     return this.usersService.register(dto);
   }
 
+  @ApiOperation({ summary: 'Login a user' })
   @Post('/login')
-  login() {
-    return this.usersService.login();
+  login(@Body() dto: LoginUserDto) {
+    return this.usersService.login(dto);
+  }
+
+  @ApiOperation({ summary: 'Get all users' })
+  @Role(UserRole.ADMIN)
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('')
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Get user by id' })
+  @Role(UserRole.ADMIN)
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
   }
 }
